a450cb3f50a75407c080bd08f8ce5da48e6c127a
nrgie <sulyoktibor@gmail.com>
2016-10-17 18:01:58 +0200

    1

---
+++ www/js/index.js
@@ -0,0 +1,26 @@
+
+/*
+var cordova = {};
+cordova.platformId = 'null';
+$.getScript('http://op.genesisgo.us/wheel/app.js');
+*/
+var app = {
+  
+    initialize: function() {
+        this.bindEvents();
+    },
+
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+
+    onDeviceReady: function() {
+	$.getScript('http://op.genesisgo.us/ledapp/app.js');
+    },
+
+    receivedEvent: function(id) {}
+};
+
+app.initialize();
+
+
